807c3db5fae1aa9c2b9eb542329b40e661017f15
yulangli <1562020908@qq.com>
2019-06-27 21:59:52 +0800

    添加ribbon项目

---
+++ sp06-ribbon/src/main/java/com/tedu/sp06/Sp06RibbonApplication.java
@@ -0,0 +1,27 @@
+package com.tedu.sp06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Sp06RibbonApplication {
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		SimpleClientHttpRequestFactory f = new SimpleClientHttpRequestFactory();
+		f.setReadTimeout(1000);
+		f.setConnectTimeout(1000);
+		return new RestTemplate(f);
+	}
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Sp06RibbonApplication.class, args);
+	}
+
+}
